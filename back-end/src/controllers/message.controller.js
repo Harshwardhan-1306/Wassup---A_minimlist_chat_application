8ff6ecf8ee7bcd6be6cca021dee119212a03914e
Harshwardhan <harshwardhandeore.8975@gmail.com>
2025-08-21 16:51:36 +0000

    Message Route implemented

---
+++ back-end/src/controllers/message.controller.js
@@ -0,0 +1,68 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.lib.js";
+
+export const getUsersForSidebar = async(req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
+
+        const filteredUser = await User.find({ _id: {$ne: loggedInUserId} }).select('-password');
+
+        res.status(200).json(filteredUser);
+
+    } catch (error) {
+        console.log('Error in getUsersForSidebar: ', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+export const getMessages = async(req, res) => {
+    try {
+        const {id: userToChatId} = req.params;
+        const myId = req.user._id;
+
+        const Messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: myId}
+            ]
+        });
+
+        res.status(200).json(Messages);
+
+    } catch (error) {
+        console.log('Error in getMessages function', error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+export const sendMessage = async(req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(imageUrl) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url
+        }
+
+        const newMessage = new Message({
+            senderId: senderId,
+            receiverId: receiverId,
+            text: text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+        // todo: realtime functionality will be implemented here with socket.io
+
+        res.status(201).json(newMessage);
+
+    } catch (error) {
+        console.log('Error in sendMessage controller', error.message);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'dotenv/config.js';
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -9,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/messages", messageRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
