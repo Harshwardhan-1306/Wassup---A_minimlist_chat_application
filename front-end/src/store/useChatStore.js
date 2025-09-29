e60801759ec53f5de15be2124439ed5bfd72ceb4
Harshwardhan <harshwardhandeore.8975@gmail.com>
2025-09-29 11:49:12 +0000

    Build sidebar component

---
+++ front-end/src/store/useChatStore.js
@@ -0,0 +1,37 @@
+import { create } from 'zustand';
+import toast from 'react-hot-toast';
+import { axiosInstance } from '../lib/axios';
+
+export const useChatStore = create((set) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUserLoading: false,
+    isMessageLoading: false,
+
+    getUsers: async() => {
+        set({ isUserLoading: true });
+        try {
+            let res = await axiosInstance.get('/messages/users');
+            set({ users: res.data });
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isUserLoading: false });
+        }
+    },
+
+    getMessages: async(userId) => {
+        set({ isMessageLoading: true });
+        try {
+            let res = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: res.data });
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isMessageLoading: false });
+        }
+    },
+
+    setSelectedUser: (selectedUser) => set({ selectedUser })
+}))
