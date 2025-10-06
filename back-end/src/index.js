ac5aa86156368709e49ac2be42a17865019032c7
Harshwardhan <harshwardhandeore.8975@gmail.com>
2025-10-06 10:03:33 +0000

    Patched

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -23,7 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../front-end/disk')));
+    app.use(express.static(path.join(__dirname, '../front-end/dist')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../front-end', 'dist', 'index.html'));
