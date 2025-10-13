43c63c09f14320f80af38be6d133f44b27d5123f
Harshwardhan <harshwardhandeore.8975@gmail.com>
2025-10-13 09:07:48 +0000

    Patched-1.3

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,18 +1,22 @@
 import express from 'express';
-import 'dotenv/config.js';
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import cors from 'cors';
 import { server, app } from './lib/socket.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
 
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json({ limit: '2mb' }));
-app.use(express.json());
+
 app.use(cookieParser());
 app.use(cors({
     origin: 'https://ubiquitous-pancake-pj9r56wqj4gxh6pqg-5173.app.github.dev',
@@ -22,12 +26,12 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../front-end/dist')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../front-end/dist")));
 
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../front-end', 'dist', 'index.html'));
-    })
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../../front-end/dist/index.html"));
+  });
 }
 
 server.listen(process.env.PORT, () => {
